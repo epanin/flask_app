fd0804d3776a001ff8b87ed18f8f40d034808731
e.panin <fath34d@gmail.com>
2024-01-16 21:45:21 +0300

    covered "Modificate form" theme

--- lesson_ws/example.py
+++ lesson_ws/example.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
+import json, uuid
 
 app = Flask(__name__)
 
@@ -7,16 +8,12 @@ def hello_world():
     print(request.headers)
     return 'Welcome to Flask\n'
 
-@app.get('/users/<id>/')
-def users_get(id):
+@app.get('/users')
+def users_get():
     return render_template(
-        'test.html', 
-        name=id
+        'test.html'
         )
 
-@app.post('/users')
-def users_post():
-    return 'Users\n', 302
 
 @app.route('/html/')
 def html():
@@ -30,3 +27,39 @@ def not_found():
 def course(id):
     return f'Course id: {id}'
 
+@app.route('/users/new')
+def users_new():
+    user = {'name': '', 'email': ''}
+    errors = {}
+
+    return render_template(
+        'users/new.html',
+        user=user, 
+        errors=errors,
+    )
+
+@app.post('/users')
+def users_post():
+    user = request.form.to_dict()
+    errors = validate(user)
+    if errors:
+        return render_template(
+            'users/new.html',
+            user=user, 
+            errors=errors
+        ), 422
+    id = 1
+    user['id'] = id
+    with open('users.txt', 'a', encoding='utf-8') as f:
+        json.dump(user, f) 
+    return redirect('/users', code=302)
+
+def validate(user_data):
+    errors = {}
+#     if not user['name']:
+#         errors['name'] = "Can't be blank"
+#
+#     # ...
+#
+#     return errors
+    return errors    
