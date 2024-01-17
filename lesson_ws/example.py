c1306a569d063fe2469bbc4ad6b10fd9e22d9339
e.panin <fath34d@gmail.com>
2024-01-17 22:16:50 +0300

    learned flashes and url_for

--- lesson_ws/example.py
+++ lesson_ws/example.py
@@ -1,31 +1,30 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for, flash, get_flashed_messages
 import json, uuid
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 
 @app.route('/')
 def hello_world():
     print(request.headers)
     return 'Welcome to Flask\n'
 
-@app.get('/users')
-def users_get():
+
+@app.get('/users/<name>')
+def users_get(name):
+    messages = get_flashed_messages(with_categories=True)
+
     return render_template(
-        'test.html'
+        'test.html',
+        name=name, 
+        messages=messages
         )
 
 
 @app.route('/html/')
 def html():
-    return render_template('/index.html')
+    return render_template('index.html')
 
-@app.route('/not_found/')
-def not_found():
-    return 'OOps', 404
-
-@app.route('/courses/<id>')
-def course(id):
-    return f'Course id: {id}'
 
 @app.route('/users/new')
 def users_new():
@@ -38,6 +37,7 @@ def users_new():
         errors=errors,
     )
 
+
 @app.post('/users')
 def users_post():
     user = request.form.to_dict()
@@ -48,18 +48,18 @@ def users_post():
             user=user, 
             errors=errors
         ), 422
-    id = 1
+    id = str(uuid.uuid4())
     user['id'] = id
-    with open('users.txt', 'a', encoding='utf-8') as f:
-        json.dump(user, f) 
-    return redirect('/users', code=302)
+    flash('New user created!', 'success')
+    with open('lesson_ws/data/users.txt', 'a', encoding='utf-8') as f:
+        json.dump(user, f)
+        f.write('\n') 
+    return redirect(url_for('users_get', name=user.get('name')), code=302)
+
 
 def validate(user_data):
     errors = {}
-#     if not user['name']:
-#         errors['name'] = "Can't be blank"
-#
-#     # ...
-#
-#     return errors
+    if not user_data['name']:
+        errors['name'] = "Can't be blank"
+
     return errors    
